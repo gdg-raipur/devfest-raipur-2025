54a04758999a8407df9d0e1d51be4b6e9277a95e
Akash-1808 <akashch1808@gmail.com>
2025-11-14 10:05:13 +0530

    added-konfhub-image-logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Footer from "./components/Footer";
 import Script from "next/script";
+import { url } from "inspector";
 
 
 // const geistSans = Geist({
@@ -16,6 +17,7 @@ import Script from "next/script";
 // });
 
 export const metadata: Metadata = {
+  
   title: "DevFest Raipur 2025 | November 15 | IIIT Naya Raipur",
   description: "Join DevFest Raipur 2025 on November 15 at IIIT Naya Raipur. Connect with industry leaders, attend workshops, and celebrate innovation with the Google Developer Group community.",
   keywords: "DevFest, DevFest Raipur, DevFest 2025, GDG Raipur, Google Developer Group, Tech Conference, Developer Event, IIIT Raipur",
@@ -27,7 +29,14 @@ export const metadata: Metadata = {
     locale: "en_US",
     url: "https://devfest-raipur.com",
     siteName: "DevFest Raipur 2025",
+    images: [{
+    url: '/devfest-gdg-raipur.png',
+    width: 1200,
+    height: 630,
+    alt: 'DevFest Raipur 2025',
+    }],
   },
+  
   twitter: {
     card: "summary_large_image",
     title: "DevFest Raipur 2025 | November 15 | IIIT Naya Raipur",
@@ -51,6 +60,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preload" href="/fonts/BRFirma-Bold.woff2" as="font" type="font/woff2" crossOrigin="anonymous"/>
+        <link rel="preload" href="/fonts/BRFirma-Regular.woff2" as="font" type="font/woff2" crossOrigin="anonymous"/>
+        <link rel="preload" href="/fonts/Ethnocentric-Regular.woff2" as="font" type="font/woff2" crossOrigin="anonymous"/>
+      </head>
       <body
         className={`font-sans antialiased top-0`}
       >
